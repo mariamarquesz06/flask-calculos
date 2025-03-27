b88e87c4f63f0a8502afc2b3382e7960f5db4032
maria marks <141273009+mariamarquesz06@users.noreply.github.com>
2025-03-27 14:58:06 -0300

    Add files via upload

--- metodo-post.py
+++ metodo-post.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import uuid
 
 app = Flask(__name__)
 
@@ -11,10 +12,43 @@ def somar():
     numero2 = dados_recebidos["numero2"]
     resultado = numero1 + numero2
     
-    historico.append({"numero1": numero1, "numero2": numero2, "resultado": resultado})
+    historico.append({
+        "id": str(uuid.uuid4()),
+        "numero1": numero1, 
+        "numero2": numero2, 
+        "resultado": resultado})
     
     return {'resultado': resultado}
 
 @app.route("/calculos", methods=["GET"])
 def calculos():
     return {'historico':historico}
+
+
+@app.route("/deletar/<id>", methods=["DELETE"])
+def deletar_item(id):
+    global historico
+    historico = [d for d in historico if d.get ('id') != id]
+
+    return{}
+
+@app.route("/editar/<id>", methods=["PUT"])
+def editar_item(id):
+    global historico
+    dados_recebidos = request.get_json()
+    numero1 = dados_recebidos["numero1"]
+    numero2 = dados_recebidos["numero2"]
+
+    lista_temp = []
+    for calculo in historico :
+        if calculo ["id"] == id:
+            resultado = numero1 + numero2
+            historico.append({
+            "id": id,
+            "numero1": numero1, 
+            "numero2": numero2, 
+            "resultado": resultado})
+        else:
+            lista_temp.append(calculo)
+    historico = lista_temp
+    return{}
